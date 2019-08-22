144f5debde0f71ffad56f5a6fdb25e233d432364
Antony Carlos <antonyc828@gmail.com>
2019-08-22 10:08:24 -0300

    add comments on metods in App class

--- src/main/java/org/grafos/teoriadosgrafos/App.java
+++ src/main/java/org/grafos/teoriadosgrafos/App.java
@@ -5,37 +5,54 @@ import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.builder.GraphTypeBuilder;
 import org.jgrapht.util.SupplierUtil;
 
-/**
- * Hello JGraphT!
- */
+
 public class App {
 	
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		
-		Graph<String, DefaultEdge> graph = GraphTypeBuilder
-				.directed()
-				.allowingMultipleEdges(true)
-				.allowingSelfLoops(true)
-				.vertexSupplier(SupplierUtil.createStringSupplier())
-				.edgeSupplier(SupplierUtil.createDefaultEdgeSupplier())
-				.buildGraph();
+        Graph<String, DefaultEdge> graph = GraphTypeBuilder
+                                           .directed()
+                                           .allowingMultipleEdges(true)
+                                           .allowingSelfLoops(true)
+                                           .vertexSupplier(SupplierUtil.createStringSupplier())
+                                           .edgeSupplier(SupplierUtil.createDefaultEdgeSupplier())
+                                           .buildGraph();
+                
+        
+        //metodos principais
+        
+        /**
+         * metodo addVertex()
+         * adiciona os vertices do grafo
+         */
+        String v0 = graph.addVertex();
+        String v1 = graph.addVertex();
+        String v2 = graph.addVertex();
 
-		String v0 = graph.addVertex();
-		String v1 = graph.addVertex();
-		String v2 = graph.addVertex();
+        /**
+         * metodo addEdge()
+         * adiciona as ligaçoes entre os vertices (aresta)
+         */
+        graph.addEdge(v0, v1);
+        graph.addEdge(v1, v2);
+        graph.addEdge(v0, v2);
 
-		graph.addEdge(v0, v1);
-		graph.addEdge(v1, v2);
-		graph.addEdge(v0, v2);
-
-		for (String v : graph.vertexSet()) {
-			System.out.println("vertex: " + v);
-		}
-
-		for (DefaultEdge e : graph.edgeSet()) {
-			System.out.println("edge: " + e);
-		}
-		
-	}
-	
+        /**
+         * metodo vertexSet()
+         * retorna os vertices contidos no grafo
+         */
+        System.out.println("Vertex:");
+        for (String v : graph.vertexSet()) {
+            System.out.println("vertex: " + v);
+        }
+        
+        /**
+         * metodo edgeSet()
+         * retorna as arestas contidos no grafo
+         */
+        System.out.println("\nEdges:");
+        for (DefaultEdge e : graph.edgeSet()) {
+            System.out.println("edge: " + e);
+        }
+    }
 }
